ba1928f0cb81ef55b265f0a914905f7f35a2b4ee
Sean Craig <seanwhcraig@gmail.com>
2026-05-07 10:15:58 -0500

    feat: compose final page with hero + 5 bloom sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
 import { PostcardHero } from "@/components/hero/PostcardHero";
 import { TicketsCTA } from "@/components/hero/TicketsCTA";
+import { SaveTheDate } from "@/components/sections/SaveTheDate";
+import { Schedule } from "@/components/sections/Schedule";
+import { Venue } from "@/components/sections/Venue";
+import { AboutWordmark } from "@/components/sections/AboutWordmark";
+import { SupportingOMA } from "@/components/sections/SupportingOMA";
 
 export default function Page() {
   return (
     <main>
       <TicketsCTA />
       <PostcardHero />
+      <SaveTheDate />
+      <Schedule />
+      <Venue />
+      <AboutWordmark />
+      <SupportingOMA />
     </main>
   );
 }
