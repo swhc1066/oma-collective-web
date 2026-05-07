b6dfac03f524f827ed9259e5a4f24813ba277b72
Sean Craig <seanwhcraig@gmail.com>
2026-05-07 10:32:21 -0500

    style: add gap before THE ARTS; right-justify THE above ARTS

--- components/hero/PostcardHero.tsx
+++ components/hero/PostcardHero.tsx
@@ -64,7 +64,7 @@ export function PostcardHero() {
         </motion.div>
 
         <div
-          className="absolute flex items-baseline gap-[0.4em] whitespace-nowrap"
+          className="absolute flex items-baseline gap-[0.9em] whitespace-nowrap"
           style={{
             left: HERO_TEXT_POS.wordmark.left,
             bottom: HERO_TEXT_POS.wordmark.bottom,
@@ -90,7 +90,7 @@ export function PostcardHero() {
               lineHeight: 0.95,
             }}
           >
-            <span className="absolute bottom-full left-0 leading-none">THE</span>
+            <span className="absolute bottom-full right-0 leading-none">THE</span>
             ARTS
           </motion.span>
         </div>
