10e6169e64631eb425cc951c183aec59135c110e
Sean Craig <seanwhcraig@gmail.com>
2026-05-07 13:40:20 -0500

    style: ease-in-out on next-circle reveal (smoother start)

--- components/home/Stage.tsx
+++ components/home/Stage.tsx
@@ -158,7 +158,7 @@ export function Stage() {
                   animate={{ clipPath: "circle(150% at 100% 100%)" }}
                   transition={{
                     layout: { duration: 0.8, ease: [0.16, 1, 0.3, 1] },
-                    clipPath: { delay: 1.5, duration: 0.9, ease: [0.16, 1, 0.3, 1] },
+                    clipPath: { delay: 1.5, duration: 1.0, ease: [0.4, 0, 0.2, 1] },
                   }}
                 >
                   {/* Centered at the visual centroid of a bottom-right
